493fe5d8d060b93536b005890f8bd6d2741633cf
Gabriel Morales <gabbomorales1@gmail.com>
2021-02-10 14:15:10 -0600

    Landing Page Main 1st Checkpoint

--- src/views/Home/components/Main/index.tsx
+++ src/views/Home/components/Main/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import Img from '../Img'
+import TitleTextButtom from '../TitleTextButtom'
 
+import wsblanding from '../../../../img/wsblanding.png'
 
   
 
@@ -9,11 +12,31 @@ const Main: React.FC = () => {
   return (
     <>
       <Container>
-
+        <StyledTitle>Getting Started</StyledTitle>
+        <StyledText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis vehicula dolor ut suscipit.</StyledText>
+        <Grid>
+          <TitleTextButtom 
+          title='Aggregator' 
+          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae augue non dui volutpat sagittis. Ut imperdiet sapien ut auctor mattis. Mauris volutpat sed quam fringilla auctor. Mauris convallis sapien at tincidunt sollicitudin. Quisque quis elementum tellus. Integer molestie commodo mi et accumsan. Phasellus egestas, quam nec cursus rhoncus, arcu ipsum lacinia turpis, sit amet aliquet nibh arcu sit amet erat. Fusce dictum ante in elementum ultricies.'
+          button='GO TO APP'/>
+          <Img src={wsblanding}/>
+        </Grid>
       </Container>
     </>
   );
 };
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const StyledTitle = styled.h3`
+  
+`
+const StyledText = styled.p`
+  
+`
 
 const Container = styled.div`
   width: 80%;
--- src/views/Home/components/TitleTextButtom/index.tsx
+++ src/views/Home/components/TitleTextButtom/index.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 
 import styled from 'styled-components'
 
-  
+interface TitleTextButtom {
+  title?: string,
+  text?: string
+  button?: string
+}
 
-const TitleTextButtom: React.FC = (props) => {
+const TitleTextButtom: React.FC<TitleTextButtom> = (props) => {
 
   return (
     <div>
-      <StyledTitle>Title</StyledTitle>
-      <StyledText>Text</StyledText>
-      <StyledButton>Button</StyledButton>
+      <StyledTitle>{props.title}</StyledTitle>
+      <StyledText>{props.text}</StyledText>
+      <StyledButton>{props.button}</StyledButton>
     </div>
   );
 };
-
+ 
 const StyledTitle = styled.h3`
   
 `
